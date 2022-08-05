ed9a31448373f9fb5aba37d67b36e70faa558bcc
MAekzer <gma312@yandex.ru>
2022-08-05 19:26:23 +0300

    Untit 1

---
+++ 5U1/Program.cs
@@ -0,0 +1,94 @@
+﻿static string Scan() => Console.ReadLine();
+
+static string ShowColor()
+{
+	Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+	var color = Scan();
+
+	if (color != null)
+	{
+		switch (color)
+		{
+			case "red":
+				Console.BackgroundColor = ConsoleColor.Red;
+				Console.ForegroundColor = ConsoleColor.Black;
+
+				Console.WriteLine("Your color is red!");
+				break;
+
+			case "green":
+				Console.BackgroundColor = ConsoleColor.Green;
+				Console.ForegroundColor = ConsoleColor.Black;
+
+				Console.WriteLine("Your color is green!");
+				break;
+
+			case "blue":
+				Console.BackgroundColor = ConsoleColor.Blue;
+				Console.ForegroundColor = ConsoleColor.Black;
+
+				Console.WriteLine("Your color is blue");
+				break;
+
+			case "yellow":
+				Console.BackgroundColor = ConsoleColor.Yellow;
+				Console.ForegroundColor = ConsoleColor.Black;
+
+				Console.WriteLine("Your color is yellow");
+				break;
+
+			case "cyan":
+				Console.BackgroundColor = ConsoleColor.Cyan;
+				Console.ForegroundColor = ConsoleColor.Black;
+
+				Console.WriteLine("Your color is cyan");
+				break;
+
+			default:
+				Console.BackgroundColor = ConsoleColor.Yellow;
+				Console.ForegroundColor = ConsoleColor.Red;
+
+				Console.WriteLine("Please choose another color! :c");
+				break;
+		}
+		return color;
+	}
+	return "0";
+}
+
+static int[] Sort()
+{
+	var result = new int[5];
+	var temp = 0;
+
+	for (int i = 0; i < result.Length; i++)
+    {
+		Console.WriteLine("Введите элемент массива номер {0}", i + 1);
+		result[i] = int.Parse(Scan());
+    }
+
+	Console.WriteLine();
+
+	for (int i = 0; i < result.Length; i++)
+	{
+		for (int j = i + 1; j < result.Length; j++)
+		{
+			if (result[j] <= result[i])
+			{
+				temp = result[i];
+				result[i] = result[j];
+				result[j] = temp;
+			}
+		}
+	}
+
+
+	foreach (var item in result)
+    {
+		Console.WriteLine(item);
+    }
+
+	return result;
+}
+
+Sort();
--- Module5+/Program.cs
+++ Module5+/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿(string Name, string[] Dishes) User;
+
+Console.Write("Введите имя: ");
+User.Name = Console.ReadLine();
+
+Console.WriteLine("Назовите свои любимые блюда");
+
+User.Dishes = new string[5];
+
+for (int i = 1; i <= 5; i++)
+{
+    Console.Write("Назовите {0}-е блюдо: ", i);
+    User.Dishes[i - 1] = Console.ReadLine();
+}
+
+Console.Write(User.Name + " ");
+for (int i = 0; i < User.Dishes.Length; i++)
+{
+    Console.Write(User.Dishes[i] + " ");
+}
