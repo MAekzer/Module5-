1966167a988d6230076fcd5daadf8136f5cd0200
MAekzer <gma312@yandex.ru>
2022-08-09 16:05:02 +0300

    Задание 5.5.3

---
+++ 5.5/Program.cs
@@ -0,0 +1,23 @@
+﻿static void Echo(string saidworld, int deep)
+{
+    string modif = saidworld;
+
+    if (modif.Length > 2)
+    {
+        modif = modif.Remove(0, 2);
+    }
+    Console.WriteLine("..." + modif);
+
+    if (deep > 1)
+    {
+        Echo(modif, deep - 1);
+    }
+}
+
+Console.WriteLine("Скажите что-нибудь");
+var phrase = Console.ReadLine();
+
+Console.WriteLine("Задайте частоту эхо");
+var deep = Convert.ToInt32(Console.ReadLine());
+
+Echo(phrase, deep);
