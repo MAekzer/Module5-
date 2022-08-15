ebae6b70818fd0765265cb592cc156420b17158d
MAekzer <gma312@yandex.ru>
2022-08-15 20:27:05 +0300

    Начат Модуль 6 Юнит 2

---
+++ U1/Program.cs
@@ -0,0 +1,26 @@
+﻿class Rectangle
+{
+    public int A;
+    public int B;
+
+    public int Square()
+    {
+        return A * B;
+    }
+
+    public Rectangle()
+    {
+        A = 6;
+        B = 4;
+    }
+    public Rectangle(int a, int b)
+    {
+        A = a;
+        B = b;
+    }
+
+    public Rectangle(int a)
+    {
+        A = B = a;
+    }
+}
