4f719f9745470df183dc0b9699afcd0cfc6cec60
MAekzer <gma312@yandex.ru>
2022-08-17 17:58:31 +0300

    2-5 юниты

---
+++ 6U3/Program.cs
@@ -0,0 +1,13 @@
+﻿class Bus
+{
+    public int? Load;
+
+    public void PrintStatus()
+    {
+        if (Load.HasValue)
+            Console.WriteLine("В автобусе {0} пассажиров", Load.Value);
+        else
+            Console.WriteLine("В автобусе нет пассажиров");
+    }
+}
+
---
+++ 6U5/Program.cs
@@ -0,0 +1,48 @@
+﻿class Triangle
+{
+    public int A;
+    public int B;
+    public int C;
+
+    public double S()
+    {
+        double p = (A + B + C) / 2;
+        double s = Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+        return s;
+    }
+
+    public int P()
+    {
+        return A + B + C;
+    }
+}
+
+class Square
+{
+    public int A;
+
+    public int S()
+    {
+        return Convert.ToInt32(Math.Pow(A, 2));
+    }
+
+    public int P()
+    {
+        return A * 4;
+    }
+}
+
+class Circle
+{
+    public double R;
+
+    public double S()
+    {
+        return R * R * 3.14;
+    }
+
+    public double P()
+    {
+        return R * 2 * 3.14;
+    }
+}
