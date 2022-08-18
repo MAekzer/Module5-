d9d83ea2e1b735d1eea4eaedc6baff9b0de50363
MAekzer <gma312@yandex.ru>
2022-08-18 20:01:05 +0300

    Закончен модуль 6

---
+++ 6U6.1/Program.cs
@@ -0,0 +1,14 @@
+﻿class TrfficLight
+{
+    public string Color;
+
+    private void ChangeColor(string color)
+    {
+        Color = color;
+    }
+
+    public string GetColor()
+    {
+        return Color;
+    }
+}
---
+++ 6U6.2/Program.cs
@@ -0,0 +1,52 @@
+﻿class User
+{
+	private int age;
+	private string mail;
+	private string username;
+
+	public string Username
+    {
+		get
+        {
+			return username;
+        }
+
+		set
+        {
+			if (value.Length >= 3)
+				username = value;
+			else
+				Console.WriteLine("Логин должен содержать не менее трех символов");
+
+        }
+    }
+
+	public string Mail
+    {
+		get
+        {
+			return mail;
+        }
+
+		set
+        {
+			if (value.Contains("@"))
+				mail = value;
+			else
+				Console.WriteLine("Некорректный формат почты");
+        }
+	}
+
+	public int Age
+	{
+		get
+		{
+			return age;
+		}
+
+		set
+		{
+			age = value;
+		}
+	}
+}
