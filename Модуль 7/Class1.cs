026540b2e20d44dfbe408d2c7a0eb07c5250d9de
MAekzer <gma312@yandex.ru>
2022-08-27 15:57:29 +0300

    Модуль 7

--- Модуль 7/Class1.cs
+++ Модуль 7/Class1.cs
@@ -1,20 +1,54 @@
 ﻿namespace Module_7
 {
-    public class Car<T>
+    public abstract class Car<TEngine> where TEngine : Engine
     {
-        public T Engine = default;
+        public TEngine Engine = default;
+
+        public abstract void ChangePart<TPart>(TPart newpart) where TPart : CarPart;
 
     }
 
-    public class ElectricEngine
+    public class GasCar<TEngine> : Car<TEngine> where TEngine : Engine
+    {
+        public override void ChangePart<TPart>(TPart newPart)
+        {
+
+        }
+    }
+
+    public class ElectricCar<TEngine> : Car<TEngine> where TEngine : Engine
+    {
+        public override void ChangePart<TPart>(TPart newPart)
+        {
+
+        }
+    }
+
+    abstract public class Engine : CarPart
     {
 
     }
 
-    public class GasEngine
+    abstract public class CarPart
     {
 
     }
+
+    public class ElectricEngine : Engine
+    {
+
+    }
+
+    public class GasEngine : Engine
+    {
+
+    }
+
+    class Battery : CarPart { }
+
+    class Differential : CarPart { }
+
+    class Wheel : CarPart { }
 
     public class Record<T1, T2>
     {
